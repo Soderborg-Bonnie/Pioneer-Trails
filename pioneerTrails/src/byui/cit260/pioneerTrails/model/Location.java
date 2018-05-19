280618772741626560992737230bc3bf0e41b66b
Harman-Tyler <cav9802@byui.edu>
2018-05-18 19:00:35 -0600

    Tyler added Map, Location, and scene classes, as well as cardinality with those they will interact with. Have not created cardinality with classes that have yet to be created.

---
+++ pioneerTrails/src/byui/cit260/pioneerTrails/model/Location.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pioneerTrails.model;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author tharman
+ */
+public class Location implements Serializable {
+    
+    //class instance variables
+    private Integer row;
+    private Integer column;
+    private Boolean visited;
+    private Integer stopsRemaining;
+    private Map map;
+    private ArrayList<Scene> scenes = new ArrayList<Scene>();
+
+    public Location() {
+    }
+
+    public ArrayList<Scene> getScenes() {
+        return scenes;
+    }
+
+    public void setScenes(ArrayList<Scene> scenes) {
+        this.scenes = scenes;
+    }
+    
+    
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+    
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    public Boolean getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Boolean visited) {
+        this.visited = visited;
+    }
+
+    public Integer getStopsRemaining() {
+        return stopsRemaining;
+    }
+
+    public void setStopsRemaining(Integer stopsRemaining) {
+        this.stopsRemaining = stopsRemaining;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", column=" + column + ", visited=" + visited + ", stopsRemaining=" + stopsRemaining + ", map=" + map + '}';
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.row);
+        hash = 71 * hash + Objects.hashCode(this.column);
+        hash = 71 * hash + Objects.hashCode(this.visited);
+        hash = 71 * hash + Objects.hashCode(this.stopsRemaining);
+        hash = 71 * hash + Objects.hashCode(this.map);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (!Objects.equals(this.row, other.row)) {
+            return false;
+        }
+        if (!Objects.equals(this.column, other.column)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        if (!Objects.equals(this.stopsRemaining, other.stopsRemaining)) {
+            return false;
+        }
+        if (!Objects.equals(this.map, other.map)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+}
