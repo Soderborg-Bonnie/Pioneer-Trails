e31ef923b4654abdb131e3782bc33fa665c4d496
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2018-05-30 19:56:18 -0600

    adding control classes

---
+++ pioneerTrails/src/byui/it260/pioneerTrails/control/ResourceControl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.it260.pioneerTrails.control;
+
+/**
+ *
+ * @author Bonnie
+ */
+public class ResourceControl {
+    
+}
