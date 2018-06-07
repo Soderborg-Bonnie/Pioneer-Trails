a398ec20377edcc6d5238384e8142a709113f05a
TamlynLau <byuitam@gmail.com>
2018-06-07 07:41:19 -1000

    Added displayStartProgramView()

--- pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
+++ pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
@@ -8,12 +8,13 @@ package byui.cit260.pioneerTrails.view;
 
 
 public class StartProgramView {
-    public StartProgramView () {
+    public StartProgramView() {
+        
     
 }
         public void displayStartProgramView() {
-            //endOfView = false
-            //DO
+            //   endOfView = false
+            //   DO
             //      inputs = getInputs()
             //      IF no inputs were entered OR the first input is Q
             //              RETURN
@@ -25,22 +26,48 @@ public class StartProgramView {
             
             do{
                 String[] inputs = this.getInputs();
+                //inputs[0].ToUpperCase();
                 if (inputs.length < 1 || inputs[0] == "Q"){
-                return ;
+                return;
                 
             }else
-                    endOfView = doAction(inputs);
-            }while(endOfView!=true);
+                endOfView = doAction(inputs);
+            }while(endOfView != true);
                    
     }
 
     private String[] getInputs() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   //    System.out.println("*** getInputs() has been called ***");
+        
+   //   String[] inputs = new String[1];
+   //     inputs[0] = "testInput";
+   
+      String[] inputs = new String[1];
+      
+      
+   
+   
+   
+        
+        return inputs;
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     private boolean doAction(String[] inputs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("***doAction() called***");
+        System.out.println("\tinputs = " + inputs[0]);
+        
+        return true;
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public void display(){
+        String[] testCode = getInputs();
+        doAction(testCode);
+    }
+    
     }
     
 
