291f30dd32a726232000463b11f1d836801e70b3
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2018-06-07 19:31:17 -0600

    starting code for getInputs()

--- pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
+++ pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
@@ -20,7 +20,7 @@ public class StartProgramView {
             //              RETURN
             //      ENDIF
             //              endOFVIEW = doAction(inputs)
-            //      WHILE endOfView != true
+            //   WHILE endOfView != true
             
             boolean endOfView = false;
             
@@ -37,22 +37,48 @@ public class StartProgramView {
     }
 
     private String[] getInputs() {
+        char[] arrayVar = new char[1];
+        
+        
+        
+        
+    }
+   //{
    //    System.out.println("*** getInputs() has been called ***");
         
    //   String[] inputs = new String[1];
    //     inputs[0] = "testInput";
+//         String[] inputs = new String[1];
+//      
+//        return inputs;
+//        
+//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//   
+//algorithm from assignment  
+//   getInputs(): String[] {
+//
+// inputs = new String array one element long
+// Display a description of the view
+//
+//valid = false
+//WHILE valid == false (no input value has been entered)
+//
+//Display the prompt message
+//Get the value entered from the keyboard
+//Trim off leading and trailing blanks from the value
+//IF length of the value < 1 then
+//Display "You must enter a non-blank value”
+//Continue (move to the top of the loop and repeat)
+//ENDIF
+//Assign the value to the fist position in the inputs array
+//valid = true (ends the loop)
+//ENDWHILE
+//
+//RETURN inputs 
    
-      String[] inputs = new String[1];
-      
-      
    
-   
-   
-        
-        return inputs;
-        
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
 
     private boolean doAction(String[] inputs) {
         System.out.println("***doAction() called***");
