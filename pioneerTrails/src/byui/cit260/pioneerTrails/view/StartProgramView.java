a6f6bc507163a6501dcc846fad89d49bda4dd4d4
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2018-06-09 14:23:31 -0600

    doAction code start

--- pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
+++ pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
@@ -15,14 +15,7 @@ public class StartProgramView {
     }
 
     public void display() {
-        //   endOfView = false
-        //   DO
-        //      inputs = getInputs()
-        //      IF no inputs were entered OR the first input is Q
-        //              RETURN
-        //      ENDIF
-        //              endOFVIEW = doAction(inputs)
-        //   WHILE endOfView != true
+       
         System.out.println("display banner");
 
         boolean endOfView = false;
@@ -58,47 +51,34 @@ public class StartProgramView {
         return inputs;
     }
 
-    //48 use toUpperCase
-    //49 create banner
-    //53 quit?
-    //char[] arrayVar = new char[0];
-    //{
-    //    System.out.println("*** getInputs() has been called ***");
-    //   String[] inputs = new String[1];
-    //     inputs[0] = "testInput";
-//         String[] inputs = new String[1];
-//      
-//        return inputs;
-//        
-//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//   
-//algorithm from assignment  
-//   getInputs(): String[] {
-//
-// inputs = new String array one element long
-// Display a description of the view
-//
-//valid = false
-//WHILE valid == false (no input value has been entered)
-//
-//Display the prompt message
-//Get the value entered from the keyboard
-//Trim off leading and trailing blanks from the value
-//IF length of the value < 1 then
-//Display "You must enter a non-blank value”
-//Continue (move to the top of the loop and repeat)
-//ENDIF
-//Assign the value to the fist position in the inputs array
-//valid = true (ends the loop)
-//ENDWHILE
-//
-//RETURN inputs 
-    private boolean doAction(String[] inputs) {
-        System.out.println("***doAction() called***");
-        System.out.println("\tinputs = " + inputs[0]);
+    
 
-        return true;
+    private boolean doAction(String[] inputs) {
+////        playersName = get the first value in the inputs array
+//player = savePlayer(playersName)
+//IF player == null
+// display “Could not create the player. “ +
+// “Enter a different name.”
+// RETURN false
+//ENDIF
+//DISPLAY "================================================= "
+// + "Welcome to the game " + playersName
+// + "We hope you have a lot of fun!”
+// + "================================================= "
+//mainMenuView = Create a new MainMenuView object
+//mainMenuView.displayMainMenuView()
+//RETURN true 
+        //System.out.println("***doAction() called***");
+       // System.out.println("\tinputs = " + inputs[0]);
+String player = savePlayer(inputs[0]);
+if (player == null)
+    System.out.println("Couldn't create player. Enter a different name.");
+return false;
+
+    System.out.println("Welcome to the game, " + player + "! We hope you have lots of fun!");
+        
+
+        //return true;
 
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
