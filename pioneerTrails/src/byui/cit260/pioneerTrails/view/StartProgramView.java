2966612819861bfb8ef4064aefe56d75f3308c4b
Harman-Tyler <38660307+Harman-Tyler@users.noreply.github.com>
2018-06-09 16:07:21 -0600

    trying to figure out savePlayer()

--- pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
+++ pioneerTrails/src/byui/cit260/pioneerTrails/view/StartProgramView.java
@@ -1,5 +1,6 @@
 package byui.cit260.pioneerTrails.view;
 
+import byui.cit260.pioneerTrails.control.GameControl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,14 +16,23 @@ public class StartProgramView {
     }
 
     public void display() {
-       
-        System.out.println("display banner");
+
+        System.out.println("\\__/\\\\\\\\\\\\\\\\\\\\\\\\\\____________________________________________________________________________________________/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_____________________________________/\\\\\\\\\\\\_________________        \n"
+                + " _\\/\\\\\\/////////\\\\\\_________________________________________________________________________________________\\///////\\\\\\/////_____________________________________\\////\\\\\\_________________       \n"
+                + "  _\\/\\\\\\_______\\/\\\\\\__/\\\\\\_________________________________________________________________________________________\\/\\\\\\_____________________________________/\\\\\\____\\/\\\\\\_________________      \n"
+                + "   _\\/\\\\\\\\\\\\\\\\\\\\\\\\\\/__\\///______/\\\\\\\\\\_____/\\\\/\\\\\\\\\\\\_______/\\\\\\\\\\\\\\\\______/\\\\\\\\\\\\\\\\___/\\\\/\\\\\\\\\\\\\\__________________\\/\\\\\\________/\\\\/\\\\\\\\\\\\\\___/\\\\\\\\\\\\\\\\\\____\\///_____\\/\\\\\\_____/\\\\\\\\\\\\\\\\\\\\_     \n"
+                + "    _\\/\\\\\\/////////_____/\\\\\\___/\\\\\\///\\\\\\__\\/\\\\\\////\\\\\\____/\\\\\\/////\\\\\\___/\\\\\\/////\\\\\\_\\/\\\\\\/////\\\\\\_________________\\/\\\\\\_______\\/\\\\\\/////\\\\\\_\\////////\\\\\\____/\\\\\\____\\/\\\\\\____\\/\\\\\\//////__    \n"
+                + "     _\\/\\\\\\_____________\\/\\\\\\__/\\\\\\__\\//\\\\\\_\\/\\\\\\__\\//\\\\\\__/\\\\\\\\\\\\\\\\\\\\\\___/\\\\\\\\\\\\\\\\\\\\\\__\\/\\\\\\___\\///__________________\\/\\\\\\_______\\/\\\\\\___\\///____/\\\\\\\\\\\\\\\\\\\\__\\/\\\\\\____\\/\\\\\\____\\/\\\\\\\\\\\\\\\\\\\\_   \n"
+                + "      _\\/\\\\\\_____________\\/\\\\\\_\\//\\\\\\__/\\\\\\__\\/\\\\\\___\\/\\\\\\_\\//\\\\///////___\\//\\\\///////___\\/\\\\\\_________________________\\/\\\\\\_______\\/\\\\\\__________/\\\\\\/////\\\\\\__\\/\\\\\\____\\/\\\\\\____\\////////\\\\\\_  \n"
+                + "       _\\/\\\\\\_____________\\/\\\\\\__\\///\\\\\\\\\\/___\\/\\\\\\___\\/\\\\\\__\\//\\\\\\\\\\\\\\\\\\\\__\\//\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\_________________________\\/\\\\\\_______\\/\\\\\\_________\\//\\\\\\\\\\\\\\\\/\\\\_\\/\\\\\\__/\\\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\\\\\\_ \n"
+                + "        _\\///______________\\///_____\\/////_____\\///____\\///____\\//////////____\\//////////__\\///__________________________\\///________\\///___________\\////////\\//__\\///__\\/////////__\\//////////__"
+        );
 
         boolean endOfView = false;
 
         do {
             String[] inputs = this.getInputs();
-           
+
             if (inputs[0].equalsIgnoreCase("Q")) {
                 return;
             } else {
@@ -51,8 +61,6 @@ public class StartProgramView {
         return inputs;
     }
 
-    
-
     private boolean doAction(String[] inputs) {
 ////        playersName = get the first value in the inputs array
 //player = savePlayer(playersName)
@@ -69,22 +77,21 @@ public class StartProgramView {
 //mainMenuView.displayMainMenuView()
 //RETURN true 
         //System.out.println("***doAction() called***");
-       // System.out.println("\tinputs = " + inputs[0]);
-String player = savePlayer(inputs[0]);
-if (player == null)
-    System.out.println("Couldn't create player. Enter a different name.");
-return false;
+        // System.out.println("\tinputs = " + inputs[0]);
+        Player player = GameControl.savePlayer(inputs[0]);
+//if (player == null)
+//    System.out.println("Couldn't create player. Enter a different name.");
+//return false;
 
-    System.out.println("Welcome to the game, " + player + "! We hope you have lots of fun!");
-        
+        System.out.println("Welcome to the game, " + player + "! We hope you have lots of fun!");
 
-        //return true;
-
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-//    public void display(){
-//        String[] testCode = getInputs();
-//        doAction(testCode);
+        return true;
+//
+//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 //    }
+//
+////    public void display(){
+////        String[] testCode = getInputs();
+////        doAction(testCode);
+    }
 }
