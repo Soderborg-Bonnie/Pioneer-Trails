58b13c46e1eb7bf44e862892930f700985693dfe
Harman-Tyler <38660307+Harman-Tyler@users.noreply.github.com>
2018-06-10 22:36:06 -0600

    it's 10:35 and alllllllls well

--- pioneerTrails/src/byui/cit260/pioneerTrails/view/GetHelp.java
+++ pioneerTrails/src/byui/cit260/pioneerTrails/view/GetHelp.java
@@ -5,15 +5,116 @@
  */
 package byui.cit260.pioneerTrails.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Bonnie
  */
-class GetHelp {
+public class GetHelp {
 
-    void displayGetHelp() {
-        System.out.println("Yay for help!");
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public GetHelp() {
     }
     
+    public void displayGetHelp(){
+        
+        boolean endOfView = false;
+
+        do {
+
+            System.out.println("===============================");
+            System.out.println(" Help Menu");
+            System.out.println("===============================");
+            System.out.println("\n*************************************");
+            System.out.println("\n* G - What is the goal of the game? *");
+            System.out.println("\n* M - How to move                   *");
+            System.out.println("\n* E - Estimate resources            *");
+            System.out.println("\n* H - Harvest resources             *");
+            System.out.println("\n* Q - Quit                          *");
+            System.out.println("\n*************************************");
+
+            String[] inputs = this.getInputs();
+
+            if (inputs[0].equalsIgnoreCase("Q")) {
+                return;
+            } else {
+                endOfView = doAction(inputs);
+            }
+        } while (endOfView != true);
+
+    }
+//   
+
+    private String[] getInputs() {
+        Scanner scanner = new Scanner(System.in);
+        String[] inputs = new String[1];
+        Boolean valid = false;
+        while (!valid) {
+            System.out.println("Please select an option: ");
+            inputs[0] = scanner.nextLine();
+            inputs[0] = inputs[0].trim();
+            if (inputs[0].length() < 1) {
+                System.out.println("try again");
+                continue;
+            }
+            valid = true;
+
+        }
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+
+        String menuItem = inputs[0].toUpperCase();
+        
+            switch (menuItem) {
+                case "G":
+                    getGameGoal();
+                    break;
+                case "M":
+                    getMoveHelp();
+                    break;
+                case "E":
+                    estimateResources();
+                    break;
+                case "H":
+                    harvestResources();
+                    break;
+                case "Q":
+                    return true;
+                default:
+                    System.out.println("You've come to the right place. You clearly need HELP. Select a valid option");
+            }
+        
+        return false;
+
+    }
+
+   
+    private void getGameGoal() {
+        GetGameGoal getGameGoal = new GetGameGoal();
+        getGameGoal.displayGetGameGoal();
+    }
+
+    private void getMoveHelp() {
+        GetMoveHelp getMoveHelp = new GetMoveHelp();
+        getMoveHelp.displayGetMoveHelp();
+    }
+
+    private void estimateResources() {
+        EstimateResources estimateResources = new EstimateResources();
+        estimateResources.displayEstimateResources();
+    }
+
+    private void harvestResources() {
+        HarvestResources harvestResources = new HarvestResources();
+        harvestResources.displayHarvestResources();
+    }
 }
+
+
+
+    
+    
+
+
