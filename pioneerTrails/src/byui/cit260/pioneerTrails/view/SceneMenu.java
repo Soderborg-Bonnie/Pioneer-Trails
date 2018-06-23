91c67889a570e09bcb0648ec518bb3068ac4a9e4
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2018-06-23 00:14:52 -0600

    fix main menu

--- pioneerTrails/src/byui/cit260/pioneerTrails/view/SceneMenu.java
+++ pioneerTrails/src/byui/cit260/pioneerTrails/view/SceneMenu.java
@@ -111,7 +111,9 @@ public class SceneMenu {
 //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+
+
     void display() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
